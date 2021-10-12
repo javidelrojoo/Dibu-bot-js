904c8f3670c49c640105ed44dcab4ea74df26952
javildelrojoo <javitau459@gmail.com>
2021-10-12 02:37:42 -0300

    era eso pero rompio todo

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,9 @@
 module.exports = {
 	name: 'messageCreate',
 	async execute(message) {
+		if (message.author.bot){
+			return
+		}
 		if (message.author.id == 470723884166021120) {
 			message.react('<:felis:887018949206753321>');
 		}
