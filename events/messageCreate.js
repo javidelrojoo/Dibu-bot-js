55c934819a95ef129c1cafe4cef4d97e4a912234
javildelrojoo <javitau459@gmail.com>
2021-10-12 02:24:22 -0300

    grande martin

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,7 +5,7 @@ module.exports = {
 		if (message.author.id == 470723884166021120) {
 			message.react('<:felis:887018949206753321>');
 		}
-		if (toLowerCase(message.content) == 'grande martin') {
+		if (message.content.toLowerCase() == 'grande martin') {
 			message.reply('grande martin');
 		}
 	},
